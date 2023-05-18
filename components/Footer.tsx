42c371ceecfe049696646a98001d133413725391
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-05-18 19:19:22 +0100

    💄 updated footer ui

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { Typography } from "@material-tailwind/react";
 
 export default function Footer() {
   return (
-    <footer className="flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between">
+    <footer className="flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between p-4">
       <Typography color="blue-gray" className="font-normal">
         &copy; 2023 Material Tailwind
       </Typography>
