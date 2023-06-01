2a91d630b585ca1788900900e113db76ca969c48
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-06-02 00:33:12 +0200

    :lipstick: create home page

--- components/NewsletterCard.tsx
+++ components/NewsletterCard.tsx
@@ -14,7 +14,7 @@ export default function NewsletterCard() {
         <Card className="flex-row w-full max-w-[48rem] shadow-xl">
             <CardHeader shadow={false} floated={false} className="w-2/5 shrink-0 m-0 rounded-r-none">
                 <img
-                    src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+                    src="/newsletter.png"
                     alt="image"
                     className="w-full h-full object-cover"
                 />
