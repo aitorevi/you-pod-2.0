f34978fae10792653318aadef9db942471138dfb
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-06-13 13:08:53 +0200

    :bug: suppressHydrationWarning, app-index.js:32 Warning: Extra attributes from the server: class,style

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ import {ThemeProvider} from "next-themes";
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
 
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
         <body className="bg-white dark:bg-gray-900">
         <Providers>
             <ThemeProvider attribute="class">
