33f981df2fc7182812198595a3dca94e28c31bb0
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-06-19 09:27:17 +0200

    :bug:  Unable podcasts for un not logged users

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import {NextResponse} from "next/server";
 import {withAuth} from "next-auth/middleware";
 
 export default withAuth(function middleware(req) {
-    const isNotAutorized = (req.nextUrl.pathname.startsWith("/admin") && req.nextauth.token?.role === "admin");
+    const isNotAutorized = (req.nextUrl.pathname.startsWith("/admin") && req.nextauth.token?.role !== "admin");
     if (isNotAutorized) {
         NextResponse.next();
         return NextResponse.redirect(new URL("/", req.url));
